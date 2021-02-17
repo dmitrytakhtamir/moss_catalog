48333bd113f365eababbb8c09f4076d86bbb33f8
dmitrytakhtamir <72991969+dmitrytakhtamir@users.noreply.github.com>
2021-02-18 02:34:21 +0300

    Update settings.py

--- moss_catalog/settings.py
+++ moss_catalog/settings.py
@@ -146,3 +146,14 @@ BOOTSTRAP4 = {
 BOOTSTRAP3 = {
 'include_jquery': True,
 }
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email@gmail.com' #С которого будет приходить сообщение
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+
+ADMINS = [
+    ('dima', 'dmitrytakhtamir@yandex.ru'),
+]
