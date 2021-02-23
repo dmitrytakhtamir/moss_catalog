d2387b1d84e9474b9bf712a2032419e394a27134
dmitrytakhtamir <72991969+dmitrytakhtamir@users.noreply.github.com>
2021-02-23 23:44:17 +0300

    Update forms.py

--- moss/forms.py
+++ moss/forms.py
@@ -4,47 +4,6 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 
-class NewSpecies(forms.ModelForm):
-	class Meta:
-		model = Species
-		fields = '__all__'
-		exclude = ['creator']
-
-class NewGenus(forms.ModelForm):
-	class Meta:
-		model = Genus
-		fields = '__all__'
-		exclude =  ['creator']
-
-class SpeciesEdition(forms.ModelForm):
-	class Meta:
-		model = Species
-		fields = '__all__'
-
-class GenusEdition(forms.ModelForm):
-	class Meta:
-		model = Genus
-		fields = '__all__'
-
-class SubclassEdition(forms.ModelForm):
-	class Meta:
-		model = Subclass
-		fields = '__all__'
-		exclude = ['creator']
-	
-	images = forms.ImageField(label=u'Фотографии', widget=forms.FileInput(attrs={'multiple': 'multiple'}), required=False)
-
-
-class NewSubclass(forms.ModelForm):
-	class Meta:
-		model = Subclass
-		fields = '__all__'
-		exclude =  ['creator']
-		
-	images = forms.ImageField(label=u'Фотографии', widget=forms.FileInput(attrs={'multiple': 'multiple'}), required=False)
-
-
-
 class AddMoreImg(forms.ModelForm):
 	class Meta: 
 		model =  SpeciesImages
